c50dbd2d3ede67b20cc6c4de4aedbd60fcb21736
Nathalie Zwimpfer <nathaliezwimpfer@Nathalies-MacBook-Pro.local>
2021-04-22 08:30:59 -0400

    Implement feature for marking task complete

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const taskElement = document.getElementById('box-1');
+const closingX = document.getElementById('closing-x');
+const elementText = document.getElementById('item');
+const checkmark = document.getElementById('checkmark');
+
 taskElement.addEventListener('click', function () {
   changeBackgroundColor();
   makeXWhite();
   makeTextWhite();
-  strikeThroughText();
   addCheckmark();
 });
 
 function changeBackgroundColor() {
-  
+  taskElement.style.backgroundColor = 'hsla(0, 0%, 30%, 1)';
+}
+
+function makeXWhite() {
+  closingX.style.color = 'white';
+}
+
+function makeTextWhite() {
+  elementText.style.color = 'white';
+  elementText.style.textDecoration  = 'line-through';
+}
+
+function addCheckmark() {
+  checkmark.style.display = 'inline';
 }
